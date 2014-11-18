cb7ed05ada767e50b5c1ead6f9e7fcc6c041ed39
allenjzhang <allenzhang@live.com>
2014-11-17 17:56:06 -0800

    Initial Checkin of LogViewer

---
+++ source/ProfileLogViewer/Src/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProfileLogViewer
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
